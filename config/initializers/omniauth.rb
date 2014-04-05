ac57673de324459bb345dc5f5e4084eb581f0397
Drew Pappas <drewpappas@mac.com>
2014-04-04 22:15:26 -0700

    Rollback

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,13 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, 'VAVpWf1VaLaYUGUKx790rg', 'qmtyJyNYOCnUdKfMHik6btPnEmfLg4dbsUfVM2i3o',
+  {
+      :secure_image_url => 'false',
+      :image_size => 'large'
+      }
+  
+  provider :facebook, '249183201915324', 'eef7bc0ef405855920d6f2e87ed2ae1d',
+  :image_size => 'large'
+    
+  provider :identity, on_failed_registration: lambda { |env| IdentitiesController.action(:new).call(env)
+  	}
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FanaticV2::Application.config.secret_key_base = '1967c7cf01a656c6d1a7216e57d4f4e57a122665970f94ffd5254cc94f950b170691ba9931b676cb91120cf7255c81ba5670b8c6baf0d9d8a3a8fc89c371600f'
