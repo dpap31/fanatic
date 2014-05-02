f27c3cd02bd8817509dd0f4b3213399a70bb9a69
Drew Pappas <drewpappas@mac.com>
2014-05-01 18:36:20 -0700

    added before filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   include PublicActivity::StoreController
+  before_action :check_sign_in
   before_action :activities
   protect_from_forgery with: :exception
 
@@ -13,6 +14,16 @@ end
 hide_action :current_user
 
 private
+
+def check_sign_in
+  unless user_signed_in?
+  redirect_to controller:'public', action: 'index'
+end
+end
+
+def user_signed_in?
+  return true if current_user
+end
 
 def activities
   @activities = PublicActivity::Activity.limit(10).order("created_at desc")
