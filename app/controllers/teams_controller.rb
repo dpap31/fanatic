772e12512e51dd9100e8f545c5c78f0a558fe00e
Drew Pappas <drewpappas@mac.com>
2014-05-18 12:20:49 -0700

    added select2 tags and reddit sharing

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -10,6 +10,15 @@ class TeamsController < ApplicationController
   	 @team = Team.new
   end
 
+   def update
+    @team = Team.find(params[:id])
+    if @team.update_attributes(params[:team])
+      redirect_to @team, notice: "Successfully created product."
+    else
+      render :edit
+    end
+  end
+
   def team_params
     params[:team].permit(team_ids: [])
   end
