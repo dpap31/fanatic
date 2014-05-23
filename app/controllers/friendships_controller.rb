314768c3ab7225c06a87d177dbf27d0c335f24ad
Drew Pappas <drewpappas@mac.com>
2014-05-22 22:31:14 -0700

    finished Follow/Unfollow button

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,7 +2,7 @@ class FriendshipsController < ApplicationController
   def create
    @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
     if @friendship.save
-      redirect_to  '/users', :notice => "Successfully created friendship."
+      redirect_to :back, :notice => "You are now following #{User.find(@friendship.friend_id).name}"
     else
        flash[:error] = "Unable to add friend."
        redirect_to root_url
@@ -12,8 +12,7 @@ class FriendshipsController < ApplicationController
   def destroy
     @friendship = current_user.friendships.find(params[:id])
     @friendship.destroy
-    flash[:notice] = "Removed friendship."
-    redirect_to current_user
+    redirect_to :back, notice: "You are no longer following #{User.find(@friendship.friend_id).name}"
   end
 
 private 
