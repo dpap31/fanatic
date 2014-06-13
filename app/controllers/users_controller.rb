e0ba92403454ceaacbabe2a60f8d087bcb6ca6c5
Drew Pappas <drewpappas@mac.com>
2014-06-12 18:05:13 -0700

    updated user controller to respond with HTML & JSON

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,10 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+     respond_to do |format|
+    format.json { render :json => User.all } 
+    format.html { render :html => @users = User.all }
+  end
   end
 
   # GET /users/1
