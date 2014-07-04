a3aacdd6984723dfeb8b1185569303e1ebbe9d7f
Drew Pappas <drewpappas@mac.com>
2014-07-03 17:13:38 -0700

    Users JS Update

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -13,7 +13,51 @@ $(document).ready(function() {
 			valid: 'glyphicon glyphicon-ok',
 			invalid: 'glyphicon glyphicon-remove',
 			validating: 'glyphicon glyphicon-refresh'
-		}
+		},
+		threshold: 3,
+		fields: {
+			'user[first_name]': {
+				validators: {
+					notEmpty: {
+						message: 'First name is required'
+					}
+				}
+			},
+			'user[last_name]': {
+				validators: {
+					notEmpty: {
+						message: 'Last name is required'
+					}
+				}
+			},
+			'user[email]': {
+                validators: {
+                    notEmpty: {
+                        message: 'The email is required and cannot be empty'
+                    },
+                    emailAddress: {
+                        message: 'The input is not a valid email address'
+                    }
+                }
+            },
+		   'user[username]': {
+                message: 'The username is not valid',
+                validators: {
+                    notEmpty: {
+                        message: 'The username is required and cannot be empty'
+                    },
+                    stringLength: {
+                        min: 4,
+                        max: 30,
+                        message: 'The username must be more than 4 and less than 30 characters long'
+                    },
+                    regexp: {
+                        regexp: /^[a-zA-Z0-9_]+$/,
+                        message: 'The username can only consist of alphabetical, number and underscore'
+                    }
+                }
+            }
+        }
 	});
 });
 
