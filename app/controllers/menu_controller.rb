dbeb71c7fe2d950331d61b9dd4551b4cd05b6f3c
Drew Pappas <drewpappas@mac.com>
2014-07-12 13:12:16 -0700

    refactored headlines page and ESPN api calls

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -3,22 +3,10 @@ class MenuController < ApplicationController
   end
   
   def headlines
-    @headlines_nba = Headline.nba.fetch('headlines')
+  	@headlines_top = Headline.top
+    @headlines_nba = Headline.nba
     @headlines_mlb = Headline.mlb
     @headlines_nhl = Headline.nhl
     @headlines_nfl = Headline.nfl
-
-    espn = EspnRb.headlines(:api_key =>ENV["ESPN_API_key"])
-
-    # @headlines = espn.all(:top)
-
-    @accordion_headlines_nba = espn.nba(:top)
-    @accordion_headlines_mlb = espn.mlb(:top)
-    @accordion_headlines_nfl = espn.nfl(:top)
-    @accordion_headlines_nhl = espn.nhl(:top)
-
-  #if @accordion_headlines_nba.nil? ||  @accordion_headlines_mlb.nil? || @accordion_headlines_nfl.nil? || @accordion_headlines_nhl.nil?
-    #redirect_to menu_path
-
   end
 end
--- app/models/headline.rb
+++ app/models/headline.rb
@@ -1,72 +1,42 @@
 class Headline	
-	def self.nba
-		base_url = "http://api.espn.com/v1/sports/"
-		params = { :disable => "audio,categories,keywords,mobileStory,related", :limit => 10, :apikey => ENV["ESPN_API_key"]}	
-		path = "/basketball/nba/news/headlines/top/"
-		uri = URI(base_url+path)
+	#Global variables for espn
+	@@base_url = "http://api.espn.com/v1/sports/"
+	@@params = { :disable => "audio,categories,keywords,mobileStory,related", :limit => 10, :apikey => ENV["ESPN_API_key"]}
+
+	#class method that calls to the espn API
+	def self.api_call(base, params, path)
+		#Create URl and Query
+		uri = URI(base + path)
 	    uri.query = URI.encode_www_form(params) 
-	    puts uri
+	    #Start request
 		request = Net::HTTP::Get.new(uri)
 		response = Net::HTTP.start(uri.hostname, uri.port) { |http| 
 			http.request(request)
 		}
-		headlines_hash = JSON.parse(response.body)
+		#Parse response
+		hash = JSON.parse(response.body)
+		#Capture headlines node
+		headlines_hash = hash.fetch('headlines')
+	end
+
+	def self.top
+		path = "/news/headlines/top"
+	    api_call(@@base_url, @@params, path)
+	end
+	def self.nba
+		path = "/basketball/nba/news/headlines/top/"
+	    api_call(@@base_url, @@params, path)
 	end
 	def self.mlb
-		base_url = "http://api.espn.com/v1/sports/"
-		params = { :disable => "audio,categories,keywords,mobileStory,related", :limit => 10, :apikey => ENV["ESPN_API_key"]}	
 		path = "/baseball/mlb/news/headlines/top/"
-		uri = URI(base_url+path)
-	    uri.query = URI.encode_www_form(params) 
-		request = Net::HTTP::Get.new(uri)
-		response = Net::HTTP.start(uri.hostname, uri.port) { |http| 
-			http.request(request)
-		}
-		headlines_hash = JSON.parse(response.body)
-		headline = headlines_hash
+		api_call(@@base_url, @@params, path)
 	end
 	def self.nfl
-		base_url = "http://api.espn.com/v1/sports/"
-		params = { :disable => "audio,categories,keywords,mobileStory,related", :limit => 10, :apikey => ENV["ESPN_API_key"]}	
 		path = "/football/nfl/news/headlines/top/"
-		uri = URI(base_url+path)
-	    uri.query = URI.encode_www_form(params) 
-		request = Net::HTTP::Get.new(uri)
-		response = Net::HTTP.start(uri.hostname, uri.port) { |http| 
-			http.request(request)
-		}
-		headlines_hash = JSON.parse(response.body)
+		api_call(@@base_url, @@params, path)
 	end
-	def self.nhl
-		base_url = "http://api.espn.com/v1/sports/"
-		params = { :disable => "audio,categories,keywords,mobileStory,related", :limit => 10, :apikey => ENV["ESPN_API_key"]}	
+	def self.nhl	
 		path = "/hockey/nhl/news/headlines/top/"
-		uri = URI(base_url+path)
-	    uri.query = URI.encode_www_form(params) 
-		request = Net::HTTP::Get.new(uri)
-		response = Net::HTTP.start(uri.hostname, uri.port) { |http| 
-			http.request(request)
-		}
-		headlines_hash = JSON.parse(response.body)
+		api_call(@@base_url, @@params, path)
 	end
 end
-
- #  include HTTParty
- #  base_uri 'http://api.espn.com/v1/sports'
-
- #  def self.all
- #  	params = { :disable => "audio,categories,keywords,mobileStory,related", :limit => 10, :apikey => ENV["ESPN_API_key"] }
-	# params.to_param
- #    response["headlines"]
- #    response = Headline.get('/news/headlines/top',
- #      :query => { :disable => "audio,categories,keywords,mobileStory,related", :limit => 10, :apikey => ENV["ESPN_API_key"] })
- #    response["headlines"]
- #  end
- 
- # # def self.basketball
- # #    response = Headline.get('/sports/basketball/nba',
- # #      :query => { :apikey => ENV["ESPN_API_key"], :enable => "title,links" })
- # #    response["nba_headlines"]
- # #  end
-
-# end
