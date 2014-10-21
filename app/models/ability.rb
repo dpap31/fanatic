f35bfaa99a9de68e8d3d34da5054f8f6f0dd9175
Drew Pappas <drewpappas@mac.com>
2014-10-20 18:35:38 -0700

    tweaked cancancan permissions fixed CSS bugs

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -14,22 +14,24 @@ include CanCan::Ability
 
   #comment/post owener (Author) permissions 
     if user.role.to_sym == :author
-      can :read, :all
-      can :create, Comment
-      can :destroy, Comment do |comment|
-        comment.try(:user) == user 
-      end  
       can :create, Post
       can :update, Post do |post|
         post.try(:user) == user 
       end
+      can :update, User, :id => user.id
       can :destroy, Post do |post|
         post.try(:user) == user 
       end
+      can :destroy, User, :id => user.id
       can :list, Post
       can :tags, Post
       can :vote, Post
       can :sort_created, Post
+      can :read, :all
+      can :create, Comment
+      can :destroy, Comment do |comment|
+        comment.try(:user) == user 
+      end
     else user.role.to_sym == :guest
       can :read, :all
     end
