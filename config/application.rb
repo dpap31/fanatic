3a4a496700be3fa4b294cbf6845007880e876df2
Drew Pappas <drewpappas@mac.com>
2015-02-07 17:26:39 -0800

    removed rails_12factor gem and added serve static assets to the gem file

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module FanaticV2
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     ENV.update YAML.load_file('config/application.yml')[Rails.env] rescue {}
+    config.serve_static_assets = true
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
